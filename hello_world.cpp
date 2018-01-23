996f8c9b7a9ebbcb92f8743b006c20267052005e
AIRobotUSA <35674826+airobotusa@users.noreply.github.com>
2018-01-22 20:28:21 -0500

    Create hello_world.cpp
    first c++ program

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+// my first c++ program
+
+/* Hello world */
+#include <iostream>
+using namespaces std;
+
+int main()
+{
+  cout << "Hello world! \n\n" << endl;
+  cout << " I love programming !";
+  return 0;
+ }
+ 
